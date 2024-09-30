05b3deda792ac2fbfa9938b11296778c35abde16
Abinaya S <abinayas495100@gmail.com>
2024-09-29 21:53:15 -0700

    Changed API

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
+
+import { useState,useEffect } from 'react';
+import {X} from 'lucide-react'
+import { editProject, getProjects } from '../service/apiService';
 import ecom from '../assets/img/ecom.jpg'
 import ai from '../assets/img/ai.jpeg'
 function Projects() {
-  const projects = [
-    {
-      title: "Project 1: E-Commerce Website",
-      description: "A full-stack e-commerce platform built with MERN stack, featuring user authentication, product browsing, and secure payments.",
-      imgUrl: ecom, 
-      // githubLink: "https://github.com/AbinayaS110505",
-    },
-    {
-      title: "Project 2:Easy Accessible AI Tool",
-      description: "An easy-to-use tool designed for seamless accessibility, offering quick interactions and a user-friendly interface.",
-      imgUrl: ai,
-      // githubLink: "https://github.com/AbinayaS110505", 
-    },
-  ];
+  
+  
+   const [projectdata, setprojectdata] = useState([]);
+ 
+
+  // const projects = [
+  //   {
+  //     title: "Project 1: E-Commerce Website",
+  //     description: "A full-stack e-commerce platform built with MERN stack, featuring user authentication, product browsing, and secure payments.",
+  //     imgUrl: ecom, 
+  //     // githubLink: "https://github.com/AbinayaS110505",
+  //   },
+  //   {
+  //     title: "Project 2:Easy Accessible AI Tool",
+  //     description: "An easy-to-use tool designed for seamless accessibility, offering quick interactions and a user-friendly interface.",
+  //     imgUrl: ai,
+  //     // githubLink: "https://github.com/AbinayaS110505", 
+  //   },
+  // ];
+  const fetchproject = async () => {
+    try {
+      const { data } = await getProjects();
+      setprojectdata(data)
+      console.log(data)
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  // useEffect(()=>{
+  //    fetchproject()
+  // },[])
+  
   return (
     <div className="flex flex-col items-center py-8 gap-6">
       <h1 className="text-4xl font-bold mb-8 gap-6">My Projects </h1>
+      <button onClick={fetchproject} className='border-2 border-blue-400 w-[8vw] hover:bg-blue-400 hover:text-white'>get</button>
+
       <div className="flex flex-wrap justify-center gap-10 px">
-        {projects.map((project, index) => (
+        {projectdata.map((project, index) => (
           <div
             key={index}
             className="flex flex-col  items-center bg-gray-100 p-7 rounded-lg h-full w-[30vw] "
@@ -31,7 +56,7 @@ function Projects() {
               className="w-full h-22 object-cover rounded-md mb-8"
             />
             <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
-            <p className="text-gray-700 mb-4">{project.description}</p>
+            <p className="text-gray-700 mb-4">{project.desc}</p>
             <div className="flex space-x-4">
               {/* <a
                 href={project.githubLink}
@@ -42,7 +67,9 @@ function Projects() {
             </div>
           </div>
         ))}
+
       </div>
+    
     </div>
   );
 }
---
+++ src/service/apiService.js
@@ -0,0 +1,12 @@
+// src/apiService.js
+import axios from 'axios';
+
+const API = "http://localhost:7777";
+
+const getProjects = () => axios.get(`${API}/projects/all`);
+const getProjectByID = (id) => axios.get(`${API}/projects/all/${id}`);
+const addProject = (projectData) => axios.post(`${API}/projects/add`, projectData);
+const editProject = (id, editeddata) => axios.put(`${API}/projects/edit/${id}`, editeddata);
+const deleteProject = (id) => axios.delete(`${API}/projects/delete/${id}`);
+
+export { getProjects, getProjectByID, addProject, editProject, deleteProject };
